797fa70e4da9989aa60cf97e8845bc150870b668
Bernardo Tomaz <bernardo.tomaz@hotmail.com>
2024-11-20 14:52:43 -0300

    Day_three

--- server.js
+++ server.js
@@ -1,50 +1,10 @@
 import express from "express";
-
-const post = [
-  {
-    id: 1,
-    descricao: "Uma foto teste 1",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Um gato fofo dormindo",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Paisagem com um gato",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 4,
-    descricao: "Gato brincando com um novelo de lã",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 5,
-    descricao: "Gato olhando para a câmera",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(3000, () => {
   console.log("Servidor rodando");
-});
-
-app.get("/post", (req, res) => {
-  res.status(200); // Status ok passado automaticamente, não é necessário escrever.
-  res.json(post);
-});
-
-function getPostById(id) {
-  return post.findIndex((post) => post.id === Number(id));
-}
-
-app.get("/post/:id", (req, res) => {
-  const id = getPostById(req.params.id);
-  res.json(post[id]);
 });
